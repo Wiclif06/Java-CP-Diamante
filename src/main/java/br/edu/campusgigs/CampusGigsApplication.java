bf27e47702634ef59d7fb5e8f44fd91d1429815e
Wiclif06 <felipewicliflealdasilva@gmail.com>
2026-09-16 22:09:06 -0300

    CP1 - Ambiente Docker e schema inicial
    Usei Docker Compose para reproduzir a API e o PostgreSQL. O Flyway mantém o schema inicial versionado e auditável.

---
+++ src/main/java/br/edu/campusgigs/CampusGigsApplication.java
@@ -0,0 +1,11 @@
+package br.edu.campusgigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGigsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampusGigsApplication.class, args);
+    }
+}
